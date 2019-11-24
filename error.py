6c909c985d4647044dab42e71765662a949d41ec
pixar0407 <pixar0407@naver.com>
2019-11-25 02:32:49 +0900

    psnr

--- error.py
+++ error.py
@@ -9,6 +9,7 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 #%matplotlib inline
 import torch.nn.functional as F
+import torch.nn as nn
 
 
 bs = 8
@@ -31,7 +32,7 @@ model = Net()
 model.to(device)
 
 model.load_state_dict(torch.load('/content/gdrive/My Drive/data/answer.ckpt', map_location="cpu")) # 경록
-
+criterion = nn.MSELoss()
 
 
 model.eval()
@@ -40,6 +41,7 @@ error_1 = 0 # RMS linear
 error_2 = 0 # RMS log
 error_3 = 0 # abs rel
 error_4 = 0 # sqr rel
+avg_psnr = 0 # ㅔpsnr
 with torch.no_grad():
     data, target = next(iter(dl))
     data, target = data.to(device), target.to(device)
@@ -49,11 +51,15 @@ with torch.no_grad():
     error_2 += model_utils.err_rms_log(output, target).item()
     error_3 += model_utils.err_abs_rel(output, target).item()
     error_4 += model_utils.err_sql_rel(output, target).item()
+    mse = criterion(output, target)
+    psnr = 10 * log10(1 / mse.item())
+    avg_psnr += psnr
 
     error_0 /= 8
     error_1 /= 8
     error_2 /= 8
     error_3 /= 8
     error_4 /= 8
+    avg_psnr /= 8
     print('test is over')
-    print(f'Test set: Average loss:{error_0:.4f} / {error_1:.4f} / {error_2:.4f} /{error_3:.4f} /{error_4:.4f}')
+    print(f'Test set: Average loss:{error_0:.4f} / {error_1:.4f} / {error_2:.4f} /{error_3:.4f} /{error_4:.4f}  /{avg_psnr:.4f}')
